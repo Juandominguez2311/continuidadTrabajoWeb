6b9f29014a6e729f95d0a5746f3e70068a5cbc8d
Juandominguez2311 <juandominguez2311@gmail.com>
2025-05-18 13:04:09 -0300

    agrega mas modificaciones a la navbar y contacto

---
+++ src/app/contacto2/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+import React from 'react';
+import Image from 'next/image';
+import { ContactUsForm } from '@/components/ContactUsForm';
+import HeroBanner from '@/components/HeroBanner';
+
+export default function Contact() {
+  return (
+    <>
+      <head>
+        <title>Perinetti e Hijos || Contacto</title>
+      </head>
+      <main>
+        <HeroBanner
+          textOverImg={true}
+          title={'Contacto'}
+          titleColor={'white'}
+          positionText={'left'}
+          alignText={'left'}
+          titleClass="px-6 font-interRegular text-xl md:text-3xl lg:text-43 leading-custom-58"
+          urlImage="/assets/img/contact/contact.jpg"
+          imgClass={'z-10 h-36 md:h-46 lg:h-56 object-cover object-right'}
+        />
+        <div className="mx-auto max-w-screen-xl font-interRegular place-items-center px-6 py-10 text-center">
+          <div className="max-w-[730px] bg-white p-8 rounded-lg shadow-md mx-auto lg:relative ">
+            <h1 className="text-regalBlue font-semibold text-25 mb-2">ENVIANOS TU MENSAJE</h1>
+            <p className="mb-8 text-nobel-600 font-normal text-xl">
+              Un asesor te estará contactando en menos de 24 hs.
+            </p>
+            <ContactUsForm />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ src/components/ContactUsForm2/index.tsx
@@ -0,0 +1,86 @@
+'use client';
+import React, { useState } from 'react';
+
+type ContactUsFormProps = {};
+
+export const ContactUsForm: React.FC<ContactUsFormProps> = () => {
+  return (
+    <div className="flex items-center justify-center ">
+      <form onSubmit={() => {}} className="mx-auto max-w-xl ">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className={'flex flex-col gap-y-4'}>
+            <div>
+              <label className="block text-left text-nobel-600 text-md font-interSemiBold mb-2">
+                Nombre:
+              </label>
+              <input
+                type="text"
+                id="firstName"
+                placeholder="ej. Juan"
+                className="w-full border border-gray-300 focus:ring-gold-butter focus:border-gold-butter p-2 rounded h-[49px]"
+                autoComplete="given-name"
+              />
+            </div>
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="lastName"
+                className="block text-base text-start font-bold font-interSemiBold leading-6 text-nobel-600"
+              >
+                Apellido:
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  id="lastName"
+                  className="w-full border border-gray-300 focus:ring-gold-butter focus:border-gold-butter p-2 rounded h-[49px]"
+                  placeholder="ej. Perez"
+                />
+              </div>
+            </div>
+            <div>
+              <label className="block text-left text-nobel-600 text-md font-interSemiBold mb-2">
+                Mail:
+              </label>
+              <input
+                type="email"
+                id="email"
+                autoComplete="email"
+                placeholder="ej. email@gmail.com"
+                className="w-full border border-gray-300 focus:ring-gold-butter focus:border-gold-butter p-2 rounded h-[49px]"
+              />
+            </div>
+            <div>
+              <label className="block text-left text-nobel-600 text-md font-interSemiBold mb-2">
+                Telefono:
+              </label>
+              <input
+                type="tel"
+                id="phone-number"
+                autoComplete="tel"
+                placeholder="Con código de área, sin 0"
+                className="w-full border border-gray-300 focus:ring-gold-butter focus:border-gold-butter p-1 rounded h-[49px]"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label className="block text-left text-nobel-600 text-md font-interSemiBold mb-2">
+              Consulta
+            </label>
+            <textarea
+              id="message"
+              placeholder="Quisiera que me contacten"
+              className="w-full border border-gray-300 focus:ring-gold-butter focus:border-gold-butter p-2 rounded flex-1 h-auto md:h-[342px]"
+              rows={4}
+            />
+          </div>
+        </div>
+        <div className="mt-10 flex justify-center">
+          <a onClick={(e) => {}}>
+            <p>Enviar Consulta</p>
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+};
